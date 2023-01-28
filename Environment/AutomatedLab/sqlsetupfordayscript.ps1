6e08a016da0f104cf9bc4d3dabedda6cce2115b4
SQLDbaWithABeard <mrrobsewell@outlook.com>
2023-01-28 11:25:07 +0000

    gMSA and SPNs

--- Environment/AutomatedLab/sqlsetupfordayscript.ps1
+++ Environment/AutomatedLab/sqlsetupfordayscript.ps1
@@ -1,5 +1,6 @@
 $secStringPassword = 'dbatools.IO1' | ConvertTo-SecureString -AsPlainText -Force
 [pscredential]$cred = New-Object System.Management.Automation.PSCredential ('theboss', $secStringPassword)
+[pscredential]$domaincred = New-Object System.Management.Automation.PSCredential ('jessandbeard\theboss', $secStringPassword)
 
 $PSDefaultParameterValues = @{
     '*Ad*:credential' = $cred
@@ -53,3 +54,69 @@ try {
     Write-PSFMessage -Level Host -Message "Creating SQLgMSA service account"
     New-ADServiceAccount -Name SQLgMSA -PrincipalsAllowedToRetrieveManagedPassword SQLInstancesgMSAs -Enabled:$true -DNSHostName SQLgMSA.jessandbeard.local -SamAccountName SQLgMSA -ManagedPasswordIntervalInDays 90
 } 
+
+$dbatoolsmodulebase = (Get-Module dbatools -ListAvailable | select -First 1 | select modulebase).modulebase
+$PsFrameworkmodulebase = (Get-Module PSFramework -ListAvailable | select -First 1 | select modulebase).modulebase
+
+$SQLHosts | ForEach-Object {
+    $session = New-PSSession -ComputerName $_ -Credential $domaincred
+    Write-Host "Copying modules over to $($_)"
+    # Copy-Item $dbatoolsmodulebase  -ToSession $session -Destination $dbatoolsmodulebase -Recurse -Force
+    #  Copy-Item $PsFrameworkmodulebase  -ToSession $session -Destination $PsFrameworkmodulebase -Recurse -Force
+}
+# Run the SQL Server as the gMSA
+$SQLHosts | ForEach-Object {
+    $message = "Update {0} to use the SQLgMSA service account" -f $_
+    Write-PSFMessage -Level Host -Message $message
+    Invoke-Command {
+        $secStringPassword = 'dbatools.IO1' | ConvertTo-SecureString -AsPlainText -Force
+        [pscredential]$cred = New-Object System.Management.Automation.PSCredential ('theboss', $secStringPassword)
+        [pscredential]$domaincred = New-Object System.Management.Automation.PSCredential ('jessandbeard\theboss', $secStringPassword)
+        if ((Get-DbaService -SqlInstance localhost -Type Engine).StartName -ne 'jessandbeard\SQLgMSA$') {
+            Update-DbaServiceAccount -ComputerName localhost -Credential $domaincred -ServiceName MSSQLSERVER -Username 'jessandbeard\SQLgMSA$'
+        } 
+    } -ComputerName $_ -Credential $domaincred
+}
+
+# Set up the SPNs
+# Thank you Jess https://jesspomfret.com/spn-troubles/
+
+$SQLHosts | ForEach-Object {
+    $SQLHost = $PSItem
+    $secStringPassword = 'dbatools.IO1' | ConvertTo-SecureString -AsPlainText -Force
+    [pscredential]$cred = New-Object System.Management.Automation.PSCredential ('theboss', $secStringPassword)
+    [pscredential]$domaincred = New-Object System.Management.Automation.PSCredential ('jessandbeard\theboss', $secStringPassword)
+    $SPNs = Test-DbaSpn -ComputerName $SQLHost -Credential $domaincred 
+    if (($Spns | Where-Object { $psitem.IsSet -eq $false }).Count -gt 0) {
+        $message = "Remove SPNs for {0}" -f $_
+        Write-PSFMessage -Level Host -Message $message
+        Invoke-Command { param($SQLHost)
+            setspn -d MSSQLSvc/$SQLHost.jessandbeard.local $SQLHost
+            setspn -d MSSQLSvc/$SQLHost.jessandbeard.local:1433 $SQLHost
+        } -ComputerName JessDC1 -Credential $domaincred -ArgumentList $SQLHost
+    } else {
+        $message = "Don't need to remove SPNs for {0}" -f $_
+        Write-PSFMessage -Level Host -Message $message
+    }
+}
+
+$SQLHosts | ForEach-Object {
+    $message = "Setting SPNs for {0}" -f $_
+    Write-PSFMessage -Level Host -Message $message
+    Invoke-Command {
+        $secStringPassword = 'dbatools.IO1' | ConvertTo-SecureString -AsPlainText -Force
+        [pscredential]$cred = New-Object System.Management.Automation.PSCredential ('theboss', $secStringPassword)
+        [pscredential]$domaincred = New-Object System.Management.Automation.PSCredential ('jessandbeard\theboss', $secStringPassword)
+
+        try {
+            Test-DbaSpn -ComputerName $Env:ComputerName -Credential $domaincred | Where-Object { $psitem.IsSet -eq $false } | Set-DbaSpn -Credential $domaincred -ErrorAction Stop -WarningAction Stop
+            $message = "SPNs on {0} set" -f $Env:ComputerName 
+            Write-PSFMessage -Level Host -Message $message
+        } catch {
+            $message = "Failed to set SPNs on {0}" -f $Env:ComputerName 
+            Write-PSFMessage -Level Error -Message $message -Exception $_.Exception
+        }
+
+    } -ComputerName $_ -Credential $domaincred
+}
+
