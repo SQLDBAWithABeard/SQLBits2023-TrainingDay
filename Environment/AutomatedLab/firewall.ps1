4e02c7e7a902afbeb483761e04abbfb197c1749a
SQLDbaWithABeard <mrrobsewell@outlook.com>
2023-02-07 08:11:39 +0000

    so there is one file with firewall setup in it

--- Environment/AutomatedLab/firewall.ps1
+++ Environment/AutomatedLab/firewall.ps1
@@ -1,7 +1,34 @@
 
-if(-not (Get-NetFirewallRule -DisplayName 'SQL Server' -ErrorAction SilentlyContinue)) {
-    Write-Host ('Adding firewall rule')
-    New-NetFirewallRule -DisplayName 'SQL Server' -Direction Inbound -Protocol TCP -LocalPort 1433
-} else {
-    Write-Host ('Firewall rule found')
+$FirewallGroups = @( 'Remote Desktop', 'Remote Event Log Management', 'Remote Service Management' )
+
+$FireWallsConfig = @(
+    @{DisplayName = 'SQL Server'
+        Direction = 'Inbound'
+        Protocol  = 'TCP'
+        LocalPort = '1433'
+    }
+)
+
+foreach ($FirewallGroup in $FirewallGroups) {
+    try {
+        Get-NetFirewallRule -DisplayGroup $FirewallGroup -Enabled true -ErrorAction Stop | Out-Null
+        $message = "{1} Already got {0}" -f $FirewallGroup , $Env:COMPUTERNAME
+        Write-Host  $message
+    } catch {
+        $message = "{1} Need to set {0}" -f $FirewallGroup , $Env:COMPUTERNAME
+        Write-Host $message
+        Set-NetFirewallRule -DisplayGroup $FirewallGroup -Enabled true -PassThru | Out-Null
+    }
+}
+foreach ($FireWallConfig in $firewallsConfig) {
+
+    if (-not (Get-NetFirewallRule -DisplayName $FireWallConfig.DisplayName -ErrorAction SilentlyContinue)) {
+        $message = "{1} Need to set {0}" -f $FireWallConfig.DisplayName , $Env:COMPUTERNAME
+        Write-Host  $message
+
+        New-NetFirewallRule @FireWallConfig
+    } else {
+        $message = "{1} Already got {0}" -f $FireWallConfig.DisplayName , $Env:COMPUTERNAME
+        Write-Host  $message
+    }
 }
