f502e952393b124ab1c56b1ac36c4c2d94c8d469
SQLDbaWithABeard <mrrobsewell@outlook.com>
2023-02-08 12:18:42 +0000

    instally things

--- Environment/AutomatedLab/labinstall.ps1
+++ Environment/AutomatedLab/labinstall.ps1
@@ -12,99 +12,120 @@ $AdminPass = 'dbatools.IO1'
 
 $CLientVM = 'POSHClient1'
 
+$Dc1 = 'JessDC1'
+$Dc2 = 'BeardDC2'
+$FileServer = 'POSHFS1'
+$DemoMachine = 'RainbowDragon'
+
 $SQLServers = @(
 
     @{
-        Role       = 'SQLServer2016'
+        Role      = 'SQLServer2016'
         # Properties = @{InstallSampleDatabase = 'true' }
-        Name       = 'Jess2016'
-        Memory     = 1GB
-        IPAddress  = '192.168.2.53'
+        Name      = 'Jess2016'
+        Memory    = 1GB
+        IPAddress = '192.168.2.53'
     }
     @{
-        Role       = 'SQLServer2017'
+        Role      = 'SQLServer2017'
         # Properties = @{InstallSampleDatabase = 'true' }
-        Name       = 'Jess2017'
-        Memory     = 1GB
-        IPAddress  = '192.168.2.55'
+        Name      = 'Jess2017'
+        Memory    = 1GB
+        IPAddress = '192.168.2.55'
     }
     @{
-        Role       = 'SQLServer2019'
+        Role      = 'SQLServer2019'
         # Properties = @{InstallSampleDatabase = 'true' }
-        Name       = 'Jess2019'
-        Memory     = 1GB
-        IPAddress  = '192.168.2.56'
+        Name      = 'Jess2019'
+        Memory    = 1GB
+        IPAddress = '192.168.2.56'
     }
     @{
-        Role       = 'SQLServer2019'
+        Role      = 'SQLServer2019'
         # Properties = @{InstallSampleDatabase = 'true' }
-        Name       = 'Beard2019AG1'
-        Memory     = 1GB
-        IPAddress  = '192.168.2.57'
+        Name      = 'Beard2019AG1'
+        Memory    = 1GB
+        IPAddress = '192.168.2.57'
     }
     @{
-        Role       = 'SQLServer2019'
+        Role      = 'SQLServer2019'
         # Properties = @{InstallSampleDatabase = 'true' }
-        Name       = 'Beard2019AG2'
-        Memory     = 1GB
-        IPAddress  = '192.168.2.58'
+        Name      = 'Beard2019AG2'
+        Memory    = 1GB
+        IPAddress = '192.168.2.58'
     }
     @{
-        Role       = 'SQLServer2019'
+        Role      = 'SQLServer2019'
         # Properties = @{InstallSampleDatabase = 'true' }
-        Name       = 'Beard2019AG3'
-        Memory     = 1GB
-        IPAddress  = '192.168.2.59'
+        Name      = 'Beard2019AG3'
+        Memory    = 1GB
+        IPAddress = '192.168.2.59'
     }
     @{
-        Role       = 'SQLServer2019'
+        Role      = 'SQLServer2019'
         # Properties = @{InstallSampleDatabase = 'true' }
-        Name       = 'Beard2019AG4'
-        Memory     = 1GB
-        IPAddress  = '192.168.2.60'
+        Name      = 'Beard2019AG4'
+        Memory    = 1GB
+        IPAddress = '192.168.2.60'
     }
 )
 
 $GenericBlankBoxes = @(
     @{
-        Name      = 'WS2022Vm1'
-        Memory    = 1GB
-        IPAddress = '192.168.2.61'
+        Name            = 'WS2022Vm1'
+        Memory          = 1GB
+        IPAddress       = '192.168.2.61'
         OperatingSystem = 'Windows Server 2022 Datacenter (Desktop Experience)'
     }
     @{
-        Name      = 'WS2019Vm1'
-        Memory    = 1GB
-        IPAddress = '192.168.2.62'
+        Name            = 'WS2019Vm1'
+        Memory          = 1GB
+        IPAddress       = '192.168.2.62'
         OperatingSystem = 'Windows Server 2022 Datacenter (Desktop Experience)'
     }
     @{
-        Name      = 'WS2022Vm2'
-        Memory    = 1GB
-        IPAddress = '192.168.2.63'
+        Name            = 'WS2022Vm2'
+        Memory          = 1GB
+        IPAddress       = '192.168.2.63'
         OperatingSystem = 'Windows Server 2022 Datacenter (Desktop Experience)'
     }
     @{
-        Name      = 'WS2019Vm2'
-        Memory    = 1GB
-        IPAddress = '192.168.2.64'
+        Name            = 'WS2019Vm2'
+        Memory          = 1GB
+        IPAddress       = '192.168.2.64'
         OperatingSystem = 'Windows Server 2022 Datacenter (Desktop Experience)'
     }
     @{
-        Name      = 'WS2022Vm3'
-        Memory    = 1GB
-        IPAddress = '192.168.2.65'
+        Name            = 'WS2022Vm3'
+        Memory          = 1GB
+        IPAddress       = '192.168.2.65'
         OperatingSystem = 'Windows Server 2022 Datacenter (Desktop Experience)'
     }
     @{
-        Name      = 'WS2019Vm3'
-        Memory    = 1GB
-        IPAddress = '192.168.2.66'
+        Name            = 'WS2019Vm3'
+        Memory          = 1GB
+        IPAddress       = '192.168.2.66'
         OperatingSystem = 'Windows Server 2022 Datacenter (Desktop Experience)'
     }
 )
 
-
+$AllMachines = $SQLServers + $GenericBlankBoxes + @(
+    @{
+        Name = $CLientVM
+    },
+    @{
+        Name = $dc1
+    }
+    @{
+        Name = $dc2
+    }
+    @{
+        Name = $FileServer
+    }
+    @{
+        Name = $DemoMachine
+    }
+)
 
 #create an empty lab template and define where the lab XML files and the VMs will be stored
 if ((Get-Lab -List) -contains $LabName) {
@@ -140,7 +161,7 @@ $PSDefaultParameterValues = @{
     'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2016 Datacenter (Desktop Experience)'
 }
 
-if ($LabDefinition.Machines | Where-Object { $_.Roles -like 'RootDC' -and $_.Name -eq 'JessDC1' }) {} else {
+if ($LabDefinition.Machines | Where-Object { $_.Roles -like 'RootDC' -and $_.Name -eq $dc1 }) {} else {
     #the first machine is the root domain controller
     $roles = Get-LabMachineRoleDefinition -Role RootDC
     #The PostInstallationActivity is just creating some users
@@ -149,7 +170,7 @@ if ($LabDefinition.Machines | Where-Object { $_.Roles -like 'RootDC' -and $_.Nam
     $postInstallActivity += Get-LabPostInstallationActivity -ScriptFileName PrepareRootDomain.ps1 -DependencyFolder $labSources\PostInstallationActivities\PrepareRootDomain
 
     $DC1Config = @{
-        Name                     = 'JessDC1'
+        Name                     = $dc1
         Memory                   = 512MB
         Roles                    = $roles
         IpAddress                = '192.168.2.10'
@@ -160,12 +181,12 @@ if ($LabDefinition.Machines | Where-Object { $_.Roles -like 'RootDC' -and $_.Nam
     Add-LabMachineDefinition @dc1Config
 }
 
-if ($LabDefinition.Machines | Where-Object { $_.Roles -like 'DC' -and $_.Name -eq 'BeardDC2' }) {} else {
+if ($LabDefinition.Machines | Where-Object { $_.Roles -like 'DC' -and $_.Name -eq $dc2 }) {} else {
     #the root domain gets a second domain controller
     $roles = Get-LabMachineRoleDefinition -Role DC
 
     $DC2Config = @{
-        Name       = 'BeardDC2'
+        Name       = $dc2
         Memory     = 512MB
         Roles      = $roles
         IpAddress  = '192.168.2.11'
@@ -175,13 +196,13 @@ if ($LabDefinition.Machines | Where-Object { $_.Roles -like 'DC' -and $_.Name -e
     Add-LabMachineDefinition @dc2Config
 }
 
-If ($LabDefinition.Machines | Where-Object { $_.Roles -like 'FileServer' -and $_.Name -eq 'POSHFS1' }) {} else {
+If ($LabDefinition.Machines | Where-Object { $_.Roles -like 'FileServer' -and $_.Name -eq $FileServer }) {} else {
     #file server
     $roles = Get-LabMachineRoleDefinition -Role FileServer
     Add-LabDiskDefinition -Name premium1 -DiskSizeInGb 128
 
     # Using SSD storage for the additional disks
-    Add-LabMachineDefinition -Name POSHFS1 -Memory 512MB -DiskName premium1 -Roles $roles -IpAddress 192.168.2.50 -AzureProperties @{StorageSku = 'StandardSSD_LRS' }
+    Add-LabMachineDefinition -Name $FileServer -Memory 512MB -DiskName premium1 -Roles $roles -IpAddress 192.168.2.50 -AzureProperties @{StorageSku = 'StandardSSD_LRS' }
 }
 
 foreach ($SQLServer in $SQLServers) {
@@ -196,6 +217,11 @@ if ($LabDefinition.Machines | Where-Object { $_.Name -eq $ClientVM }) {} else {
     #Development client in the child domain a with some extra tools
     Add-LabMachineDefinition -Name $ClientVM -Memory 1GB -IpAddress 192.168.2.54
 }
+if ($LabDefinition.Machines | Where-Object { $_.Name -eq $DemoMachine }) {} else {
+    #a
+    #Development client in the child domain a with some extra tools
+    Add-LabMachineDefinition -Name $DemoMachine -Memory 1GB -IpAddress 192.168.2.67 -OperatingSystem 'Windows 11 Pro' -AzureProperties @{ RoleSize = 'Standard_DS3_v2' }
+}
 
 foreach ($GenericHost in $GenericBlankBoxes) {
     if ($LabDefinition.Machines | Where-Object { $_.Name -eq $GenericHost.Name }) {} else {
@@ -205,34 +231,52 @@ foreach ($GenericHost in $GenericBlankBoxes) {
 
 
 Install-Lab -Verbose
+Install-Lab -Verbose
 
 Show-LabDeploymentSummary -Detailed
 
-foreach($SQLServer in $SQLServers){
-    $ActivityName = "SQL Start for {0}" -f  $SQLServer.Name
+# add inbound firewall rule for TCP 1433 and remote management
+foreach ($Machine in $AllMachines) {
+    $ActivityName = "firewall rules for {0}" -f $Machine.Name
+    Invoke-LabCommand -FilePath .\Environment\AutomatedLab\firewall.ps1 -ComputerName $Machine.Name -DoNotUseCredSsp -ActivityName $ActivityName
+}
+
+foreach ($SQLServer in $SQLServers) {
+    $ActivityName = "SQL Start for {0}" -f $SQLServer.Name
     Invoke-LabCommand -FilePath .\Environment\AutomatedLab\startsql.ps1 -ComputerName $SQLServer.Name -DoNotUseCredSsp -ActivityName $ActivityName
 }
 
-# add inbound firewall rule for TCP 1433
-foreach($SQLServer in $SQLServers){
-    $ActivityName = "SQL Server inbound firewall rules for {0}" -f  $SQLServer.Name
-    Invoke-LabCommand -FilePath .\Environment\AutomatedLab\firewall.ps1 -ComputerName $SQLServer.Name -DoNotUseCredSsp -ActivityName $ActivityName
+
+foreach ($VM in @($DemoMachine, $CLientVM)) {
+    $scripts = @(
+        @{
+            filePath     = '.\Environment\AutomatedLab\chocoinstall.ps1'
+            ActivityName = 'Chocolatey Install'
+        },
+        @{
+            filePath     = '.\Environment\AutomatedLab\code-setup.ps1'
+            ActivityName = 'VS Code Setup'
+        },
+        @{
+            filePath     = '.\Environment\AutomatedLab\modules.ps1'
+            ActivityName = 'Install Modules'
+        }
+    )
+    foreach ($script in $scripts) {
+        $Message = 'Executing: {0} on {1}' -f $script.filePath, $VM
+        Write-PSFMessage -Message $Message -Level Host
+        Invoke-LabCommand -FilePath $script.filePath -ComputerName $VM -DoNotUseCredSsp -ActivityName $script.ActivityName
+    }
+    Write-PSFMessage -Message ('Executing: {0}' -f $_.Name) -Level Output
+    Invoke-LabCommand -FilePath .\Environment\AutomatedLab\chocoinstall.ps1 -ComputerName $VM -DoNotUseCredSsp -ActivityName 'Chocolatey Install'
 }
 
-Invoke-LabCommand -FilePath .\Environment\AutomatedLab\chocoinstall.ps1 -ComputerName $ClientVM -DoNotUseCredSsp -ActivityName 'Chocolatey Install'
-
-Restart-LabVM -ComputerName $ClientVM
-
-Invoke-LabCommand -FilePath .\Environment\AutomatedLab\code-setup.ps1 -ComputerName $ClientVM -DoNotUseCredSsp -ActivityName 'VS Code Setup'
-
-Invoke-LabCommand -FilePath .\Environment\AutomatedLab\modules.ps1 -ComputerName $ClientVM -DoNotUseCredSsp -ActivityName 'Install Modules'
-
 Get-ChildItem .\Environment\AutomatedLab\SqlSetup -File | Sort-Object Name | ForEach-Object {
-    Write-PsfMessage -Message ('Executing: {0}' -f $_.Name) -Level Output
+    Write-PSFMessage -Message ('Executing: {0}' -f $_.Name) -Level Output
     Invoke-LabCommand -FilePath $_.FullName -ComputerName $ClientVM -DoNotUseCredSsp -ActivityName 'SQLSetUP'
 }
 
-Invoke-LabCommand -FilePath .\Environment\AutomatedLab\fileserversetup.ps1 -ComputerName POSHFS1 -DoNotUseCredSsp -ActivityName 'FileServer'
+Invoke-LabCommand -FilePath .\Environment\AutomatedLab\fileserversetup.ps1 -ComputerName $FileServer -DoNotUseCredSsp -ActivityName 'FileServer'
 
 Invoke-LabCommand -FilePath .\Environment\AutomatedLab\SQLBackupsSetup.ps1 -ComputerName $ClientVM -DoNotUseCredSsp -ActivityName 'SQLBackup Set up'
 
