598aeefe97c47e262cef52a22969c9b871ae4638
SQLDbaWithABeard <mrrobsewell@outlook.com>
2023-02-10 17:32:00 +0000

    so that we ge tthe variables in one place and into the prompt

--- Environment/AutomatedLab/global/01-variables.ps1
+++ Environment/AutomatedLab/global/01-variables.ps1
@@ -1,8 +1,8 @@
-$global:secStringPassword = 'dbatools.IO1' | ConvertTo-SecureString -AsPlainText -Force
-[pscredential]$global:cred = New-Object System.Management.Automation.PSCredential ('theboss', $secStringPassword)
-[pscredential]$global:domaincred = New-Object System.Management.Automation.PSCredential ('jessandbeard\theboss', $secStringPassword)
+$global:__secStringPassword = 'dbatools.IO1' | ConvertTo-SecureString -AsPlainText -Force
+[pscredential]$global:__cred = New-Object System.Management.Automation.PSCredential ('theboss', $__secStringPassword)
+[pscredential]$global:__domaincred = New-Object System.Management.Automation.PSCredential ('jessandbeard\theboss', $__secStringPassword)
 
-$global:SASQLHosts = 'Jess2016', 'Jess2017', 'Jess2019'
-$global:SQLAgs = 'Beard2019AG1','Beard2019AG2','Beard2019AG3','Beard2019AG4'
+$global:__SASQLHosts = 'Jess2016', 'Jess2017', 'Jess2019'
+$global:__SQLAgs = 'Beard2019AG1','Beard2019AG2','Beard2019AG3','Beard2019AG4'
 
-$global:SQLHosts = $SASQLHosts + $SQLAgs
+$global:__SQLHosts = $__SASQLHosts + $__SQLAgs
