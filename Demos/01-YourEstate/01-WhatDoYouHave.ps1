a0c9b1e06fdeea103bef98435ec58a7ae3ca4a7a
SQLDbaWithABeard <mrrobsewell@outlook.com>
2023-02-16 17:18:30 +0000

    finding the things

--- Demos/01-YourEstate/01-WhatDoYouHave.ps1
+++ Demos/01-YourEstate/01-WhatDoYouHave.ps1
@@ -1,4 +1,22 @@
 # Find all the SQL Instances on your estate
+Get-ADComputer -Filter * | Sort-Object Name | ForEach-Object { Resolve-DnsName -Name $_.Name }
+
+Find-DbaInstance -DiscoveryType All -OutVariable svrs
+
+Find-DbaInstance -DiscoveryType Domain -DomainController jessdc1
+
+$AdComputerNames = (Get-adcomputer -Filter * ).Name
+
+$SQLInstances = $AdComputerNames | Find-DbaInstance
+
+$AvailableSQL = $SqlInstances | ForEach-Object {
+    Write-PSFMessage -Level Host -Message "Testing connection to $($_.SqlInstance)"
+    $results = Test-DbaConnection -SqlInstance $_.SqlInstance -SkipPSRemoting -WarningAction SilentlyContinue
+    if ($results.ConnectSuccess) {
+        Write-PSFMessage -Level Host -Message "SUCCESS - Testing connection to $($_.SqlInstance)"
+        $_
+    }
+}
 
 # Save the results to excel - Because everyone wants excel !!
 
