6aa163faf9a537199aa4f120f0c1d1e36b466980
jpomfret <jpomfret7@gmail.com>
2023-02-25 10:46:00 +0000

    patching demo

--- Demos/03-Patching/01-AreWePatched.ps1
+++ Demos/03-Patching/01-AreWePatched.ps1
@@ -1,31 +1,53 @@
 # KBUpdate
 
-# Import the module
+# Import the module - https://github.com/potatoqualitee/kbupdate
 Import-Module kbupdate
 
 # View the commands
 Get-Command -Module kbupdate
 
 # Get the updates needed for a computer
-Get-KbNeededUpdate -ComputerName jess2016
+Get-KbNeededUpdate -ComputerName jess2019 -OutVariable patches
+
+# Also see this with dbatools
+Test-DbaBuild -SqlInstance jess2016,jess2017,jess2019 -Latest |
+Format-Table SqlInstance, BuildLevel, BuildTarget, SupportedUntil, Compliant
 
 # set this to the update you want to use
-$kb = 'KB459208'
+$kb = 'KB5023049' # SQL Server 2019 RTM Cumulative Update (CU) 19 KB5023049
 
 # KB5023363 - Get information about the update
 Get-KbUpdate -Name $kb -Simple
 
-# Is it installed on our computers?
-Get-KbInstalledUpdate -ComputerName jess2016 -Name $kb
-
-# Patch the computer with the needed updates
-Get-KbNeededUpdate -ComputerName jess2016 | Install-KbUpdate
+# Patch the computer with the needed updates - 15mins to install
+Install-KbUpdate -ComputerName Jess2019 -HotfixId $kb -Verbose
 
 # reboot
-Restart-Computer -ComputerName jess2016 -Force -Wait
+Restart-Computer -ComputerName Jess2019 -Force -Wait
+
+# we good?
+Get-KbInstalledUpdate -ComputerName Jess2019 -Name $kb
+
+# Also see this with dbatools
+Test-DbaBuild -SqlInstance jess2016,jess2017,jess2019 -Latest |
+Format-Table SqlInstance, BuildLevel, BuildTarget, SupportedUntil, Compliant
 
 # Actually uninstall the update
-Uninstall-KbUpdate -ComputerName jess2016 -Name $kb -Verbose
+Uninstall-KbUpdate -ComputerName Jess2019 -Name $kb -Verbose
 
-# Is it installed on our computers?
-Get-KbInstalledUpdate -ComputerName jess2016 -Name $kb
+<#
+ComputerName : Jess2019
+Title        : SQL Server 2019 RTM Cumulative Update (CU) 19 KB5023049
+ID           : KB5023049
+Status       : Install successful
+FileName     : sqlserver2019-kb5023049-x64_a0df7db34758ce47d81286df13fd3d396c4abf51.exe
+
+VERBOSE: [08:50:42][Start-JobProcess] Finished installing updates on Jess2019
+#>
+
+# reboot
+Restart-Computer -ComputerName Jess2019 -Force -Wait
+
+# Also see this with dbatools - we're no longer compliant
+Test-DbaBuild -SqlInstance jess2016,jess2017,jess2019 -Latest |
+Format-Table SqlInstance, BuildLevel, BuildTarget, SupportedUntil, Compliant
