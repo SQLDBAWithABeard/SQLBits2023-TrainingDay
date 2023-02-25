e721bdd2b42722d181c02331d1c544b36d54b8aa
jpomfret <jpomfret7@gmail.com>
2023-02-25 10:46:19 +0000

    ag - but maybe old now

--- Environment/AutomatedLab/SqlSetup/06-ag.ps1
+++ Environment/AutomatedLab/SqlSetup/06-ag.ps1
@@ -18,34 +18,29 @@ $PSDefaultParameterValues = @{
 #Get-DbaAgHadr -SqlInstance Beard2019AG1 -SqlCredential  $domaincred
 
 $SQLAgs | ForEach-Object {
+    $sqlagnode = $psitem
     $message = ("Setting hadr option for {0}" -f $_)
     Write-PSFMessage -Level Host -Message $message
-    Invoke-Command {
-        $secStringPassword = 'dbatools.IO1' | ConvertTo-SecureString -AsPlainText -Force
-        [pscredential]$cred = New-Object System.Management.Automation.PSCredential ('theboss', $secStringPassword)
-        [pscredential]$domaincred = New-Object System.Management.Automation.PSCredential ('jessandbeard\theboss', $secStringPassword)
 
-        try {
-            Write-PSFMessage -Level Host -Message ('hadr section' -f $Env:ComputerName)
-            <#
-            if(-not (Get-DbaAgHadr -SqlInstance $Env:ComputerName).IsHadrEnabled) {
-                Write-PSFMessage -Level Host -Message ('hadr not enabled on {0} - enabling...' -f $Env:ComputerName)
+    try {
+        Write-PSFMessage -Level Host -Message ('hadr section' -f $sqlagnode)
 
-                # force restarts the instance
-                Enable-DbaAgHadr -SqlInstance $Env:ComputerName -Force -Confirm:$false
-            } else {
-                Write-PSFMessage -Level Host -Message ('hadr already enabled on {0}' -f $Env:ComputerName)
+        if(-not (Get-DbaAgHadr -SqlInstance $sqlagnode).IsHadrEnabled) {
+            Write-PSFMessage -Level Host -Message ('hadr not enabled on {0} - enabling...' -f $sqlagnode)
 
-            }
-            #>
-        } catch {
-            $message = "Failed to set hadr option on {0}" -f $Env:ComputerName
-            Write-PSFMessage -Level Error -Message $message -Exception $_.Exception
+            # force restarts the instance
+            Enable-DbaAgHadr -SqlInstance $sqlagnode -Confirm:$false
+
+            Invoke-Command -scriptblock { get-service MSSQLSERVER | Restart-Service -Force } -cn $sqlagnode -Credential $domaincred
+        } else {
+            Write-PSFMessage -Level Host -Message ('hadr already enabled on {0}' -f $sqlagnode)
         }
+    } catch {
+        $message = "Failed to set hadr option on {0}" -f $sqlagnode
+        Write-PSFMessage -Level Error -Message $message -Exception $_.Exception
+    }
 
-    } -ComputerName $_ -Credential $domaincred
 }
-#endregion
 
 #region createAg
 
@@ -57,28 +52,41 @@ if(Get-DbaAvailabilityGroup -SqlInstance $primaryAgReplica -AvailabilityGroup $A
     Write-PSFMessage -Level Host -Message ('AG exists on {0}' -f $primaryAgReplica)
 } else {
     Write-PSFMessage -Level Host -Message ('Creating the {0} AG on {1}' -f $agName, $primaryAgReplica)
+
+
     $AvailabilityGroupConfig = @{
         Name         = $AgName
         SharedPath   = '\\poshfs1\SQLBackups\Shared'
         Primary      = $primaryAgReplica
         Secondary    = $secondaryAgRelica
-        ClusterType  = 'None' # External. Wsfc
-        SeedingMode  = 'Automatic' # or you guessed it - Manual
-        FailoverMode = 'Manual' # or automatic or External
+        ClusterType  = 'Wsfc' # External. Wsfc. None
+        FailoverMode = 'Automatic' # or automatic or External
         Confirm      = $false
+        IpAddress    = '192.168.2.68'
     }
-    New-DbaAvailabilityGroup @AvailabilityGroupConfig
+    New-DbaAvailabilityGroup @AvailabilityGroupConfig -verbose
 }
 
-#endregion
+if((Get-DbaAgDatabase -SqlInstance $primaryAgReplica -AvailabilityGroup $AgName | Measure-Object).Count -ge 5) {
+    Write-PSFMessage -Level Host -Message ('AG has 5 databases already')
+} else {
+    # add 5 random databases to the AG
+    $dbs = Get-Random -Count 5 (Get-DbaDatabase -SqlInstance $primaryAgReplica -ExcludeSystem -ExcludeDatabase ReportServer, ReportServerTempDB)
+    $dbs | Set-DbaDbRecoveryModel -RecoveryModel Full -Confirm:$false
+    $agDatabaseConfig = @{
+        SqlInstance       = $primaryAgReplica
+        AvailabilityGroup = $AgName
+        Database          = $dbs.Name
+        Secondary         = $secondaryAgRelica
+        SeedingMode       = 'Manual'
+        SharedPath        = '\\poshfs1\SQLBackups\Shared'
+
+    }
+    Add-DbaAgDatabase @agDatabaseConfig
+}
 
 
 
-#region add listener
-# Add-DbaAgListener -SqlInstance $server -AvailabilityGroup MikeyAG -IPAddress $listener -Name AGListener
-#endregion
-
-#region add databases
-# Add-DbaAgDatabase -SqlInstance $server -AvailabilityGroup MikeyAG -Database NewDB4AG -Secondary $server2
-#endregion
-
+#TODO: IF replicas > 3 then boot out beard2019ag4
+#TODO: IF more than 5 databases boot some out
+#TODO: IF ag isn't running on node 1 put it back
