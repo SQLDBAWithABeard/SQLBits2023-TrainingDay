7e52f10399e1a5fbfeb3ff634efd557193c814dc
jpomfret <jpomfret7@gmail.com>
2023-02-25 11:06:42 +0000

    more test

--- Demos/00- Reset and Validate/99-Validate.Tests.ps1
+++ Demos/00- Reset and Validate/99-Validate.Tests.ps1
@@ -1,7 +1,7 @@
 BeforeDiscovery {
     $ADComputers = Get-ADComputer -Filter * -Properties Name | Select-Object -ExpandProperty Name
     $FileShare = '\\poshfs1\SQLBackups'
-
+    $SQLServers = 'Jess2016', 'Jess2017', 'Jess2019'
 }
 Describe "All Machines" {
     Context "Validating <_>" -ForEach $ADComputers {
@@ -15,6 +15,25 @@ Describe "File share" {
     Context "Validating <_>" -ForEach $FileShare {
         It "<_> should be accessible" -Tag Morning {
             (Invoke-Command -scriptblock { Get-SmbShare -Name SQLBackups } -ComputerName POSHFS1).Name | Should -Be 'SQLBackups'
+        }
+    }
+}
+
+Describe "Jess2019 should be behind on a patch" {
+    Context "Validating <_>" -ForEach @('Jess2019') {
+        It "<_> should be behind on a patch" -Tag Morning {
+            (Test-DbaBuild -SqlInstance $_ -Latest).Compliant | Should -Be $false
+        }
+    }
+}
+
+Describe "SQL servers should have 25 online databases" -Tag test {
+    Context "Validating <_>" -ForEach $SQLServers {
+        It "<_> should have 25 databases" -Tag Morning {
+            ( Get-DbaDatabase -SqlInstance $_ -ExcludeSystem | Measure-Object ).Count | Should -Be 25
+        }
+        It "<_> should have all the databases online" -Tag Morning {
+            ( Get-DbaDatabase -SqlInstance $_ -ExcludeSystem | Where-Object Status -ne 'Normal') | Should -BeNullOrEmpty
         }
     }
 }
