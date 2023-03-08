2e1b8f179881db6ce2bd9b2d094004edb2e85b0a
jpomfret <jpomfret7@gmail.com>
2023-03-08 08:35:56 +0000

    moar cleaning up

--- Demos/00- Reset and Validate/01-Reset.ps1
+++ Demos/00- Reset and Validate/01-Reset.ps1
@@ -7,3 +7,23 @@ Remove-DbaDatabase -SqlInstance beard2019ag1 -Database Pubs, Titan -Confirm:$fal
 
 # drop the NewTable from Titan on Jess2017
 Invoke-DbaQuery -SqlInstance Jess2017 -Database Titan -Query 'DROP TABLE NewTable'
+
+# AG cleanup
+# remove 4th node
+$agSplat = @{
+    SqlInstance       = 'Beard2019AG1'
+    AvailabilityGroup = 'DragonAg'
+}
+Remove-DbaAgReplica @agSplat -Replica Beard2019AG4 -Confirm:$false
+
+# Remove databases from 4th node
+Get-DbaDatabase -sqlinstance Beard2019AG4 -Status Restoring | Remove-DbaDatabase -Confirm:$false
+
+# remove pubs & titan from ag
+Remove-DbaAgDatabase -SqlInstance Beard2019AG1 -AvailabilityGroup DragonAg -Database Pubs, Titan -Confirm:$false
+
+# remove pubs & titan databases from ag nodes
+Get-DbaDatabase -SqlInstance beard2019ag1, Beard2019AG2, Beard2019AG3 -Database Pubs, Titan | Remove-DbaDatabase -Confirm:$false
+
+# disable hadr from node 4
+Disable-DbaAgHadr -SqlInstance Beard2019AG4 -Force
